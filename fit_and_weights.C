5cf24ce67093bdd328f63a2fae216ed9c00a9e45
apmorris <s1202957@sms.ed.ac.uk>
2016-02-13 16:48:49 +0100

    mostly working

--- fit_and_weights.C
+++ fit_and_weights.C
@@ -14,7 +14,7 @@ void fit_and_weights(){
 
     gROOT->ProcessLine(".x ~/cern/scripts/lhcbStyle.C");
     //lhcbStyle();
-    gStyle->SetLabelSize(0.05,"x");
+    /*gStyle->SetLabelSize(0.05,"x");
     gStyle->SetLabelSize(0.05,"y");
     gStyle->SetTitleSize(0.05,"x");
     gStyle->SetPaperSize(20,26);
@@ -22,11 +22,11 @@ void fit_and_weights(){
     gStyle->SetPadRightMargin(0.05); // increase for colz plots
     gStyle->SetPadBottomMargin(0.0);
     gStyle->SetPadLeftMargin(0.14);
-    gStyle->SetTitleH(0.01);
+    gStyle->SetTitleH(0.01);*/
                                                                                     //
     const std::string filename("/afs/cern.ch/work/a/apmorris/private/cern/ntuples/new_tuples/signal_samples/Lb2chicpK_2011_2012_signal_withbdt_cut_08.root");           
     const std::string treename = "withbdt";                                         
-    const std::string out_file_mass("~/cern/plots/fitting/Lb2chicpK_2011_2012_mass_fit_after_bdtg_08.png");                                   
+    const std::string out_file_mass("~/cern/plots/fitting/Lb2chicpK_2011_2012_mass_fit_after_bdtg_08.pdf");                                   
                                                                                     //
 
     TFile* file = TFile::Open( filename.c_str() );
@@ -37,7 +37,8 @@ void fit_and_weights(){
 
 
     // -- signal, mass shape
-    RooRealVar Lambda_b0_DTF_MASS_constr1("Lambda_b0_DTF_MASS_constr1","m(#chi_{c}pK^{-})", 5550., 5700., "MeV/c^{2}"); 
+    RooRealVar Lambda_b0_DTF_MASS_constr1("Lambda_b0_DTF_MASS_constr1","m(#chi_{c}pK^{-})", 5550., 5700., "MeV/c^{2}");
+    //RooRealVar Lambda_b0_DTF_MASS_constr2("Lambda_b0_DTF_MASS_constr2","m(#chi_{c}pK^{-})", 5550., 5700., "MeV/c^{2}"); 
     RooRealVar Jpsi_M("Jpsi_M","m(#mu#mu)", 3000., 3200., "MeV/c^{2}"); 
     RooRealVar chi_c_M("chi_c_M","m(J/#psi#gamma)", 3400., 3700., "MeV/c^{2}"); 
     RooRealVar mean("mean","mean", 5630., 5610., 5650.);
@@ -48,10 +49,11 @@ void fit_and_weights(){
     RooRealVar alpha2("alpha2","alpha2", -0.5, -5.5, 0.0);
     RooRealVar n2("n2","n2", 0.7, 0.2, 10.0);
     //RooRealVar bkgcat_chic("bkgcat_chic","bkgcat_chic", 0, 100);
-    RooRealVar bdtg3("bdtg", "bdtg", -1.0, 1.0);                                    //
+    RooRealVar bdtg("bdtg", "bdtg", -1.0, 1.0);                                    //
     RooRealVar frac2("frac2","frac2", 0.3, 0., 1.);
     
     Lambda_b0_DTF_MASS_constr1.setBins(75);
+    //Lambda_b0_DTF_MASS_constr2.setBins(75);
     
     RooRealVar proton_ProbNNp("proton_ProbNNp","proton_ProbNNp",0.0,1.0);
     RooRealVar proton_ProbNNk("proton_ProbNNk","proton_ProbNNk",0.0,1.0);
@@ -69,26 +71,26 @@ void fit_and_weights(){
     RooRealVar bgYield("bgYield","bg Yield", 1e2, 1e0, 5e5);
 
     /* 8/2/16
-     EXT PARAMETER                                INTERNAL      INTERNAL
+    EXT PARAMETER                                INTERNAL      INTERNAL
   NO.   NAME      VALUE            ERROR       STEP SIZE       VALUE
-   1  alpha1       1.95196e+00   5.61003e-02   2.56658e-03  -3.62575e-01
-   2  alpha2      -1.97943e+00   4.74197e-02   2.83267e-03   2.84008e-01
-   3  frac2        6.14816e-01   2.67461e-02   3.79093e-03   2.31700e-01
-   4  mean         5.61971e+03   1.71676e-02   2.25218e-04  -1.01505e-01
-   5  n1           2.34308e+00   9.38480e-02   2.67929e-03  -7.90059e-01
-   6  n2           3.27041e+00   2.01566e-01   8.31475e-03  -3.82656e-01
-   7  sigma1       3.84171e+00   5.81416e-02   4.35788e-04  -1.23031e+00
-   8  sigma2       6.48664e+00   1.51833e-01   1.74564e-04  -1.42244e+00
+   1  alpha1       2.50957e+00   5.68360e-02   2.65700e-03  -1.07061e-01
+   2  alpha2      -2.28664e+00   5.52765e-02   3.09340e-03  -6.11388e+00
+   3  frac2        6.51083e-01   2.02553e-02   3.33095e-03   3.06964e-01
+   4  mean         5.61967e+03   1.61655e-02   2.24983e-04  -1.02914e-01
+   5  n1           1.18728e+00   8.03720e-02   2.61213e-03  -1.04832e+00
+   6  n2           1.97964e+00   1.78120e-01   7.39156e-03  -6.90351e-01
+   7  sigma1       3.88071e+00   4.77592e-02   4.00488e-04  -1.22796e+00
+   8  sigma2       7.03034e+00   1.36260e-01   1.79652e-04  -1.41525e+00
     */
     //put in values from fit_MC here                                                //
  
-    alpha1.setVal( 1.95196e+00 );                                                   //
-    alpha2.setVal( -1.97943e+00  );                                                 //
-    n1.setVal( 2.34308e+00 );                                                       //
-    n2.setVal( 3.27041e+00 );                                                       //
-    frac2.setVal( 6.14816e-01 );                                                    //
-    sigma1.setVal( 3.84171e+00 );                                                   //
-    sigma2.setVal( 6.48664e+00 );                                                   //
+    alpha1.setVal( 2.50957e+00 );                                                   //
+    alpha2.setVal( -2.28664e+00 );                                                 //
+    n1.setVal( 1.18728e+00 );                                                       //
+    n2.setVal( 1.97964e+00 );                                                       //
+    frac2.setVal( 6.51083e-01 );                                                    //
+    sigma1.setVal( 3.88071e+00 );                                                   //
+    sigma2.setVal( 7.03034e+00 );                                                   //
                                                                                     
     alpha1.setConstant( true );
     alpha2.setConstant( true );
